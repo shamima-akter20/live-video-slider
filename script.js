19901510767b803ef02564ff97b347cddd81696c
shamima-akter20 <shamimamirpur2@gmail.com>
2023-07-08 17:35:47 +0600

    again commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click", () =>  {
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
+
+
+//javascript for video slider animation
+
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".video-slide");
+const contents = document.querySelectorAll(".content");
+
+var slideNav = function(manual){
+    btns.forEach((btn) => {
+        btn.classList.remove("active");
+    });
+
+
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+
+
+    contents.forEach((content) => {
+        content.classList.remove("active");
+    });
+
+    
+    btns[manual].classList.add("active");
+    slides[manual].classList.add("active");
+    contents[manual].classList.add("active");
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        slideNav(i);
+    })
+});
